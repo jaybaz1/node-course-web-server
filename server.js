608be5f2c1964bcf9f7309f045091e4130b8608a
Baz Jay <jaybaz1@gmail.com>
2018-01-07 21:18:20 +0000

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const pug = require('pug');
 
+const port = process.env.PORT || 3000;
+
 let app = express();
 
 app.set('view engine', 'pug');
@@ -27,6 +29,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
