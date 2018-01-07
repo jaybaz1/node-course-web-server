596947bc18468480f229786bc50f1380e23caf31
Baz Jay <jaybaz1@gmail.com>
2018-01-07 21:57:11 +0000

    Playing around with express middleware

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const pug = require('pug');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
 let app = express();
 
 app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintence.pug', {
+//         pageTitle: 'Updating site!'
+//     })
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 app.locals.currentYear = new Date().getFullYear();
